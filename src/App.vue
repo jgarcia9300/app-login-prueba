e86d8195a742eea2236b1104d869a9f30e0bf0a8
user <email@example.com>
2023-02-20 17:21:39 -0500

    integracion vuetify y toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,31 @@
 <!-- //interfaz -->
 <template>
-<div id="app">
-<section class="hero">
-<nav class="navbar is-white" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://www.univalle.edu.co">
-      <img src="https://www.univalle.edu.co/images/logo.jpg" width="245" height="300">
-    </a>
-    
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
+
+<v-app>
+  <v-toolbar density="compact">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-img
+        class="mx-2"
+        src="/src/assets/uvlogo.png"
+        max-height="40"
+        max-width="40"
+        contain
+      ></v-img>
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+     
+    </v-toolbar>
+  <v-content>
+    <div class="poto">
+      <v-btn  variant="outlined" v-on:click="fetch">Consultar</v-btn>
+        <div v-for="character of characters" v-bind:key="character.id">
+      {{ character }}
+     </div>
   </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item">
-        Home
-      </a>
-
-      <a class="navbar-item">
-        Acceder
-      </a>
-
-      
-      </div>
-    </div>
-  
-
- 
-</nav>
-</section>
-
-  
-  <button v-on:click="fetch">Consultar</button>
-
-  <div v-for="character of characters" v-bind:key="character.id">
-   {{ character }}
-   
-  </div>
-</div>
+  </v-content>
+</v-app>
 </template>
 
 
@@ -79,8 +63,7 @@ export default {
 
 </script>
 
-<style lang="scss">
-@import "~bulma/css/bulma.css"
-
+<style>
 
 </style>
+
